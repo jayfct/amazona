6cc96b86d0638dd3b87115ea6a620ac1b41db378
jay <jayfct@gmail.com>
2022-06-27 17:36:52 +0800

    Video-13-Create Product-and-Rating-Component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useReducer, useState } from "react";
-import { Link } from "react-router-dom";
+import Product from "../components/Product";
 // import data from "../data";
 import axios from "axios";
 import logger from "use-reducer-logger";
+import { Col, Row } from "react-bootstrap";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -46,23 +47,13 @@ const HomeScreen = () => {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>$ {product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
