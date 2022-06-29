80c0f514d298e9f2e5728a231f2c1adbeb19b1c8
jay <jayfct@gmail.com>
2022-06-29 09:59:35 +0800

    Video-25-Complete-Signin-Screen

--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Button,
   Container,
@@ -8,13 +9,52 @@ import {
   FormLabel,
 } from "react-bootstrap";
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Store } from "../store";
+import { getError } from "../utils";
 
 const SignInScreen = () => {
+  const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get("redirect");
   const redirect = redirectInUrl ? redirectInUrl : "/";
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/users/signin", {
+        email,
+        password,
+      });
+      ctxDispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate(redirect || "/");
+      // console.log(data);
+      //data we get back when entered pw & email same as registerd one
+      //from the post request
+      // _id: user._id,
+      // name: user.name,
+      // email: user.email,
+      // isAdmin: user.isAdmin,
+      // token: generateToken(user),
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
 
   return (
     <Container className="small-container">
@@ -22,15 +62,23 @@ const SignInScreen = () => {
         <title>Sign In </title>
       </Helmet>
       <h1 className="my-3">Sign in </h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <FormGroup className="mb-3" controlId="email">
           <FormLabel>Email</FormLabel>
-          <FormControl type="email" required></FormControl>
+          <FormControl
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></FormControl>
         </FormGroup>
 
         <FormGroup className="mb-3" controlId="password">
           <FormLabel>Password</FormLabel>
-          <FormControl type="password" required></FormControl>
+          <FormControl
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></FormControl>
         </FormGroup>
 
         <div className="mb-3">
